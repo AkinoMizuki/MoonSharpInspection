9ed0c2f9a493fb2d75c20384c1f7a684039fa016
AkinoMizuki <92746915+AkinoMizuki@users.noreply.github.com>
2022-07-20 14:44:22 +0900

    Delete LuaTest directory

--- LuaTest/main_lua/types.lua
+++
@@ -1,8 +0,0 @@
----------------------------------------
--- types
----------------------------------------
----API test
----@class WinForm
----@field sleep fun(ms: nuber): void
----@field LED fun(on_off: boolean): void
-WinForm = {}
